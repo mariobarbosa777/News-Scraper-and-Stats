9737a5ea6aa7e8e4da692d27881e06baec46fb88
Mario Barbosa <marioabarbosam@gmail.com>
2020-06-17 23:26:10 -0500

    Config.py Added

---
+++ config.py
@@ -0,0 +1,12 @@
+import yaml
+import datetime
+
+_config = None
+
+def get_config():
+    global _config
+    if not _config:
+        with open('./config.yaml') as f:
+            _config = yaml.safe_load(f)
+            _config['today'] = datetime.date.today().strftime('%Y-%m-%d')
+    return _config
